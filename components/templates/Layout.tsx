add2a6ee318e945c3fbf473fb809dcd6ff8b404a
movedoormisaki <misaki@movedoor.jp>
2022-06-27 17:10:59 +0900

    tag_maneger

--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -93,8 +93,8 @@ const Layout = ({ children }: Props) => {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MSJ9S5R');
@@ -104,6 +104,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </Head>
       {/* <Transition /> */}
       <Header nav={nav_items} link={link_items}></Header>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MSJ9S5R"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+
       <motion.div style={{ background: '#000', overflow: 'hidden', position: 'relative' }}>{children}</motion.div>
       <Footer nav={nav_items} link={link_items}></Footer>
     </>
