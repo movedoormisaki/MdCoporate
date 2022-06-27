68bce66d1230f5e53bd7f0e5f99e9b9b96e9eb4a
movedoormisaki <misaki@movedoor.jp>
2022-06-27 17:00:03 +0900

    tag_maneger

--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -90,6 +90,17 @@ const Layout = ({ children }: Props) => {
         `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MSJ9S5R');
+        `,
+          }}
+        />
       </Head>
       {/* <Transition /> */}
       <Header nav={nav_items} link={link_items}></Header>
